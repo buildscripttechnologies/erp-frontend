aff4c3d19ba70efb0d2d224b38bf3ccd066f60f1
Nisarg Mangukiya <65584994+mangukianisarg@users.noreply.github.com>
2025-08-04 10:40:02 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:5000/api", // <-- correct port for your backend
-  // baseURL: "https://api.smartflow360.com/api",
-  baseURL: "https://backend-45z3.onrender.com/api", // <-- correct port for your backend
+  baseURL: "https://api.smartflow360.com/api",
+  // baseURL: "https://backend-45z3.onrender.com/api", // <-- correct port for your backend
 });
 
 instance.interceptors.request.use(
